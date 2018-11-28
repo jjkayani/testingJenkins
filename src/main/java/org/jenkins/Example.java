dfa5243e5c9113874ccae38a92a6b8b01c7080c7
jjkayani <jamaljavedkayani@gmail.com>
2018-11-28 22:13:19 +0400

    second commit

---
+++ src/main/java/org/jenkins/Example.java
@@ -0,0 +1,21 @@
+package org.jenkins;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+//@SpringBootApplication
+public class Example {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
+
+}
